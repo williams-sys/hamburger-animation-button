6b91fec42a368c1a5b433253ce9192adf051048f
williams-sys <73890115+williams-sys@users.noreply.github.com>
2021-02-24 20:13:18 +0630

    Hamburger Animation Button

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+//first
+var Bars = document.getElementById("bars");
+    Bars.addEventListener("click", () => {
+    Bars.classList.toggle("rotate-bars");
+});
+
+//second
+var Bar = document.getElementById("bar");
+Bar.addEventListener("click", () => {
+    Bar.classList.toggle("rotate-bar");
+})
